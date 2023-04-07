3623552a66013b4687d5b462bdaa7404a93caa5c
Himanshu tyagi <himanshutyagi0158@gmail.com>
2023-04-07 22:26:12 +0530

    update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import { Navbar } from "./Components/Navbar"
+import { MainGamePage } from "./Pages/MainGamePage";
 
 const App: React.FC = () => {
     return (
-        <div className="App">
+        <div className="App h-[100vh] bg-[url('../public/WordHuntBackgroundLowOpacity.png')] bg-cover bg-center ">
             <Navbar />
+            <MainGamePage />
         </div>
     );
 }
