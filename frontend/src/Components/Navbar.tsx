e11ae01c004d60b32a39b69d5e067e105da179a2
Himanshu tyagi <himanshutyagi0158@gmail.com>
2023-04-07 10:31:34 +0530

    update

---
+++ frontend/src/Components/Navbar.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export const Navbar: React.FC = () => {
+    return (
+        <div>
+            <h1>Word-Hunt</h1>
+            <div>
+                <h2>UserName</h2>
+                <div>
+                    <h3>Game rules</h3>
+                    <h3>Logout</h3>
+                </div>
+            </div>
+        </div>
+    )
+}
