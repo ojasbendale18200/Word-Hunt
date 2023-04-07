aedaf4ee29962309e544f8622fca30f7bfe9370d
mak-1997 <107460834+mak-1997@users.noreply.github.com>
2023-04-07 13:28:20 +0530

    removed cors error

--- frontend/src/Pages/Login.tsx
+++ frontend/src/Pages/Login.tsx
@@ -1,6 +1,11 @@
 import React from "react";
+// import {io} from 'socket.io-client';
 
 const Login = () => {
+
+  // const socket = io('http://localhost:4321');
+  // socket.emit("connection",{id: 1234, name : "mayank"})
+
   return <div>Login</div>;
 };
 
