4dc01f31deefd05179c261e08b3f67e70a7548b5
Himanshu tyagi <himanshutyagi0158@gmail.com>
2023-04-08 13:09:22 +0530

    game page finalization

--- frontend/src/Components/GameCard.tsx
+++ frontend/src/Components/GameCard.tsx
@@ -1,18 +1,25 @@
 import React from 'react';
+import "../Styles/Universal.css"
 
 interface propsTypes {
-    handleChange: Function
+    handleChange: Function,
+    handleSubmit: Function,
+    score: number,
+    letter: string
 }
-export const GameCard: React.FC<propsTypes> = ({ handleChange }) => {
+export const GameCard: React.FC<propsTypes> = ({ handleChange, handleSubmit, score, letter }) => {
     return (
-        <div className={"w-[60vw] border flex-col m-auto p-10 rounded-3xl"}>
-            <h1 className={"mb-10"}>Player - {""}</h1>
-            <div className='mb-10 grid grid-cols-3 gap-10'>
-                <input className='px-4 py-2 text-black rounded-xl outline-none' type='text' placeholder='Country' onChange={(e) => { handleChange(e, "country") }} />
-                <input className='px-4 py-2 text-black rounded-xl outline-none' type='text' placeholder='Name' onChange={(e) => { handleChange(e, "name") }} />
-                <input className='px-4 py-2 text-black rounded-xl outline-none' type='text' placeholder='Animal' onChange={(e) => { handleChange(e, "animal") }} />
+        <div id='glass' className={"w-[60vw] border flex-col m-auto p-10 rounded-3xl"}>
+            <div className='hover:cursor-default flex justify-between'>
+                <h1 className={"mb-10 text-2xl"}>First Letter : {letter}</h1>
+                <h1 className={"mb-10 text-2xl"}>Score - {score}</h1>
             </div>
-            <button className={""}>Submit</button>
+            <div className='mb-10 grid grid-cols-3 gap-10'>
+                <input className='px-4 text-xl py-2 bg-transparent border-b-2 placeholder-white outline-none border-[#0099FF] text-white focus:shadow-2xl focus:shadow-[#0099FF]' type='text' placeholder='Country' onChange={(e) => { handleChange(e.target.value, "country") }} />
+                <input className='px-4 text-xl py-2 bg-transparent border-b-2 placeholder-white outline-none border-[#0099FF] text-white focus:shadow-2xl focus:shadow-[#0099FF]' type='text' placeholder='Name' onChange={(e) => { handleChange(e.target.value, "name") }} />
+                <input className='px-4 text-xl py-2 bg-transparent border-b-2 placeholder-white outline-none border-[#0099FF] text-white focus:shadow-2xl focus:shadow-[#0099FF]' type='text' placeholder='Animal' onChange={(e) => { handleChange(e.target.value, "animal") }} />
+            </div>
+            <a id='button' onClick={() => { handleSubmit() }} className='w-[30%] block m-auto text-center'><span>Submit</span><i></i></a>
         </div>
     )
 }
--- frontend/src/Pages/Signup.tsx
+++ frontend/src/Pages/Signup.tsx
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import { register } from "../utils/types";
 import axios, { AxiosResponse } from "axios";
 import { useNavigate } from "react-router-dom";
+import "../Styles/Universal.css"
 
 const Signup = () => {
   const initData = {
@@ -46,9 +47,9 @@ const Signup = () => {
   };
   return (
     <div>
-      <div className="bg-gray-100 min-h-screen flex flex-col">
+      <div className=" min-h-screen flex flex-col">
         <div className="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2">
-          <div className="bg-white px-6 py-8 rounded shadow-md text-black w-full">
+          <div id="glass" className=" px-6 py-8 rounded shadow-md text-black w-full">
             <h1 className="mb-8 text-3xl text-center">Sign up</h1>
             <form>
               <input
