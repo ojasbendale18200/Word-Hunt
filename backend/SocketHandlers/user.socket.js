d64ce790114bf5dfc17253e6c67932793ce38b51
mak-1997 <107460834+mak-1997@users.noreply.github.com>
2023-04-08 16:48:58 +0530

    added game room code for the server

--- backend/SocketHandlers/user.socket.js
+++ backend/SocketHandlers/user.socket.js
@@ -1,6 +1,7 @@
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const { UserModel } = require("../Models/user.model");
+const { GameModel } = require("../Models/game.model");
 
 
 
@@ -28,10 +29,33 @@ const userSocketHandler = (io, socket) => {
                 const inviteFrom = await UserModel.findOne({ socketId: socket.id });
                 // emit the invitation to the user
                 io.to(userToInvite.socketId).emit("invitationForYou", { invitedBy: inviteFrom.name });
-                socket.on("invitationResponseFromOpponent", (responseFromOpponent) => {
+                socket.on("invitationResponseFromOpponent", async (responseFromOpponent) => {
                     if (responseFromOpponent) {
                         io.to(inviteFrom.socketId).emit("invitationResponse", { "msg": "invitation accepted" });
+
+                        // change the status of both the user from available ---> in-game
+                        inviteFrom.status = "in-game";
+                        inviteFrom.save();
+                        userToInvite.status = "in-game";
+                        userToInvite.save();
                         // create a game in gameRoom
+                        const player_1 = {
+                            _id: inviteFrom._id,
+                            name: inviteFrom.name,
+                            score: 0,
+                            socketId: inviteFrom.socketId,
+                        };
+                        const player_2 = {
+                            _id: userToInvite._id,
+                            name: userToInvite.name,
+                            score: 0,
+                            socketId: userToInvite.socketId,
+                        };
+                        const game = await createGame(player_1, player_2);
+
+                        // send the game to both the players 
+                        io.to(player_1.socketId).emit("gameCreated", game);
+                        io.to(player_2.socketId).emit("gameCreated", game);
                     }
                     else {
                         io.to(inviteFrom.socketId).emit("invitationResponse", { "msg": "invitation rejected" });
@@ -48,4 +72,19 @@ const userSocketHandler = (io, socket) => {
 
     });
 }
+
+const createGame = async (player_1, player_2) => {
+    const game = new GameModel({
+        winner_socketId: "",
+        winner_name: "",
+        winner_score: 0,
+        player_1,
+        player_2
+    });
+    const savedGame = await game.save();
+
+    return savedGame;
+}
+
+
 module.exports = userSocketHandler;
