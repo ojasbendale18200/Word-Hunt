568a890811dc5e3f9f10ea756b89c97a15811a61
Himanshu tyagi <himanshutyagi0158@gmail.com>
2023-04-09 16:10:59 +0530

    update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import { useState, useEffect } from "react";
 import { Navbar } from "./Components/Navbar"
 import AllRoutes from "./Pages/AllRoutes";
 import io, { Socket } from "socket.io-client";
@@ -7,10 +8,29 @@ const socket = io("http://localhost:4321");
 
 
 const App: React.FC = () => {
+    const [animation, setAnimation] = useState(true);
+    useEffect(() => {
+        setTimeout(() => {
+            setAnimation(false);
+        }, 3000)
+    }, [])
     return (
         <div className="App relative">
-            <Navbar />
-            <AllRoutes socket={socket} />
+            {
+                animation ?
+                    <div id='welcomeAnimation' className='bg-[#27282c] h-[80vh] flex flex-col justify-center gap-14 items-center'>
+                        <div className='text-white text-5xl'>Welcome To</div>
+                        <div className='flex'>
+                            <img src='https://i.ibb.co/ZGfvJtZ/WORD.png' alt='Something went wrong' />
+                            <img src='https://i.ibb.co/cNrv8pq/HUNT.png' alt='Something went wrong' />
+                        </div>
+                    </div>
+                    :
+                    <>
+                        <Navbar />
+                        <AllRoutes socket={socket} />
+                    </>
+            }
         </div>
     );
 };
--- frontend/src/Pages/MatchHistory.tsx
+++ frontend/src/Pages/MatchHistory.tsx
@@ -1,9 +1,45 @@
 import React from 'react';
+import "../Styles/Universal.css";
 
 export const MatchHistory = () => {
+    interface matchData {
+        playerScore: number,
+        opponentName: string,
+        opponentScore: number
+    }
+    const matchData: matchData[] = [];
     return (
-        <div>
-
+        <div id='glass' className='flex flex-col hover:cursor-default text-white w-[60%] m-auto mt-10 border-solid gap-3 border-2 h-[75vh]'>
+            <p className='text-center text-3xl my-4'>Match History</p>
+            <div className='grid grid-cols-2 text-center text-2xl'>
+                <div className='grid grid-cols-1 border-r-2'>
+                    <p>Your score</p>
+                </div>
+                <div className='grid grid-cols-2'>
+                    <p className='border-r-2'>Opponent</p>
+                    <p>Score</p>
+                </div>
+            </div>
+            <div className='flex flex-col gap-5 overflow-x-auto'>
+                {
+                    matchData[0] ?
+                        matchData.map(e => {
+                            return (
+                                <div className='grid grid-cols-2 text-center text-xl'>
+                                    <div className='grid grid-cols-1'>
+                                        <p>{e.playerScore}{e.playerScore > e.opponentScore && "🥳"}</p>
+                                    </div>
+                                    <div className='grid grid-cols-2'>
+                                        <p>{e.opponentName}</p>
+                                        <p>{e.opponentScore}{e.playerScore < e.opponentScore && "🥳"}</p>
+                                    </div>
+                                </div>
+                            )
+                        })
+                        :
+                        <p className='text-center text-xl'>Not matches played</p>
+                }
+            </div>
         </div>
     )
 }
